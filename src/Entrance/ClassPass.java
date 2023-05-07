f294d572834f3c245c2f91f2644f4151bed27604
Furkan Karakus <furkankarakus01@hotmail.com>
2023-05-07 16:31:53 +0300

    New homework done.

---
+++ src/Entrance/ClassPass.java
@@ -0,0 +1,58 @@
+package Entrance;
+
+import java.util.Scanner;
+
+public class ClassPass {
+    public static void main(String[] args) {
+        /***       PASS THE CLASS     ***/
+
+        // Defined Variables
+        int math, physics, chemistry, history, turkish, counter = 0;
+        double average = 0;
+
+        // Get input from user
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Select your math note : ");
+        math = input.nextInt();
+        System.out.print("Please Select your physics note : ");
+        physics = input.nextInt();
+        System.out.print("Please Select your chemistry note : ");
+        chemistry = input.nextInt();
+        System.out.print("Please Select your history note : ");
+        history = input.nextInt();
+        System.out.print("Please Select your turkish note : ");
+        turkish = input.nextInt();
+
+
+        // Output
+        if ((math >= 0 && math <= 100)){
+            average +=math;
+            counter += 1;
+        }
+        if ((physics >= 0 && physics <= 100)){
+            average +=physics;
+            counter += 1;
+        }
+        if ((chemistry >= 0 && chemistry <= 100)){
+            average +=chemistry;
+            counter += 1;
+        }
+        if ((history >= 0 && history <= 100)){
+            average +=history;
+            counter += 1;
+        }
+        if ((turkish >= 0 && turkish <= 100)){
+            average +=turkish;
+            counter += 1;
+        }
+        average = average / counter;
+
+        if (average >= 55){
+            System.out.println("Congratulations You Passed The Class\nYour Average Note : " + average);
+        }else {
+            System.out.println("Sorry You Can Not Pass The Class\nYour Average Note : " + average);
+        }
+        System.out.println("Correct Note : " + counter +"\nIncorrect Note : " + (5-counter));
+    }
+    }
+
