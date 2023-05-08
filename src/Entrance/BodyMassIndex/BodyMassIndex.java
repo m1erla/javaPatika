076b2e11bfe0118a6de813e6869b91a269fd0977
mierla <116915007+m1erla@users.noreply.github.com>
2023-05-09 00:40:51 +0300

    Add files via upload

---
+++ src/Entrance/BodyMassIndex/BodyMassIndex.java
@@ -0,0 +1,26 @@
+package Entrance;
+
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    public static void main(String[] args) {
+        /***       CALCULATOR OF BODY MASS INDEX       ***/
+
+        // Defined Variables
+        int weight;
+        double  height,bodyMassIndex;
+
+        // Get input from user
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your height (in meters) :");
+        height = input.nextFloat();
+        System.out.print("Please enter your weight (in kg) : ");
+        weight = input.nextInt();
+
+        // Body mass index formul
+        bodyMassIndex = weight / (height * height);
+
+        // Output
+        System.out.print("Your Body Mass Index : " + bodyMassIndex);
+    }
+}
