7838a9baa52b26de4eda806e735eece2448d417b
Furkan Karakus <furkankarakus01@hotmail.com>
2023-05-09 14:38:53 +0300

    New homework done.

---
+++ src/Entrance/Harmonic.java
@@ -0,0 +1,21 @@
+package Entrance;
+
+import java.util.Scanner;
+
+public class Harmonic {
+    public static void main(String[] args) {
+        // Defined Variables
+        int number;
+        double result = 0.0;
+        // Get Input From User
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter A Number :  ");
+        number = input.nextInt();
+        // Loop
+        for (int i = 1; i<=number; i++){
+            result += (1/i);
+        }
+        // Output
+        System.out.println(result);
+    }
+}
