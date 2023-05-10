a198409310d6cbbfe6c406a1b6d27427c0f14cc6
mierla <116915007+m1erla@users.noreply.github.com>
2023-05-10 19:10:22 +0300

    Add files via upload

---
+++ src/Entrance/Home-Work-Eight/InvertedTriangle.java
@@ -0,0 +1,26 @@
+package Entrance;
+
+import java.util.Scanner;
+
+public class InvertedTriangle {
+    public static void main(String[] args) {
+        // Defined Variables
+        int digitsNumber;
+
+        // Get Input From User
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter A Digits Number : ");
+        digitsNumber = input.nextInt();
+
+        // Loops and Output
+        for (int i = 1; i <= digitsNumber; i++){
+            for (int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for (int k = (2 * (digitsNumber - i))+ 1; k > 0; k--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
