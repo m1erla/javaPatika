ad717afb5e62468439c3e06dcb7cff4a1cf32132
mierla <116915007+m1erla@users.noreply.github.com>
2023-05-12 14:36:22 +0300

    Add files via upload

---
+++ src/Week-Two/Second-HomeWork/RecursivePattern.java
@@ -0,0 +1,40 @@
+package WeekTwo;
+
+import java.util.Scanner;
+
+public class RecursivePattern {
+    /*** DECREASONG AND INCREASING NUMBER ***/
+    // Defined Static Method And Variables
+    static void decreasongNumber(int number, int tempNumber){
+        // Condition And Formula
+        if (number > 0){
+            System.out.print(number + " ");
+            number -= 5;
+            decreasongNumber(number, tempNumber);
+
+        }else {
+            increasingNumber(number, tempNumber);
+        }
+    }
+    // Defined Static Method And Variables
+    static void increasingNumber(int number, int tempNumber){
+        // Condition And Formula
+        if (number < tempNumber){
+            System.out.print(number + " ");
+            number += 5;
+            increasingNumber(number, tempNumber);
+        }
+    }
+    public static void main(String[] args) {
+        // Defined Variables
+        int number, tempNumber;
+        // Get Input From User
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter N Number : ");
+        number = input.nextInt();
+        // Initiliazed tempNumber
+        tempNumber = number;
+        // Output
+        decreasongNumber(number, tempNumber);
+    }
+}
