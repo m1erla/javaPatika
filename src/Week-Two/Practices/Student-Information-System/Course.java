bd456aa4ee0ec7ce25ec1c84f2831a7d94d29146
mierla <116915007+m1erla@users.noreply.github.com>
2023-05-16 21:16:35 +0300

    Add files via upload

---
+++ src/Week-Two/Practices/Student-Information-System/Course.java
@@ -0,0 +1,32 @@
+package WeekTwo;
+
+public class Course {
+    Teacher courseTeacher;
+    String name;
+    String code;
+    String match;
+    boolean isMatch;
+    int note;
+    public Course(String name, String code, boolean isMatch){
+        this.name = name;
+        this.code = code;
+        this.isMatch = isMatch;
+        this.note = 0;
+    }
+    public void addTeacher(Teacher teacher){
+        if (this.isMatch == true){
+            this.match = teacher.branch;
+            this.courseTeacher = teacher;
+            System.out.println("Transaction successful");
+        }else {
+            System.out.println(teacher.name + " Academician cannot teach this course.");
+        }
+    }
+    public void printTeacher(){
+        if (courseTeacher != null){
+            System.out.println(this.name + " Academician of the course : " + courseTeacher.name);
+        }else {
+            System.out.println(this.name + " No Academician has been assigned to the course." );
+        }
+    }
+}
---
+++ src/Week-Two/Practices/Student-Information-System/Student.java
@@ -0,0 +1,67 @@
+package WeekTwo;
+
+public class Student {
+    // Attributes
+    String name;
+    String studentNo;
+    int classes;
+    Course math;
+    Course physics;
+    Course chemistry;
+    double average;
+    boolean isPass;
+
+    // Methods
+    Student(
+            String name, String studentNo,int classes, Course math, Course physics,
+            Course chemistry)
+    {
+        this.name = name;
+        this.studentNo = studentNo;
+        this.classes = classes;
+        this.math = math;
+        this.chemistry = chemistry;
+        this.physics= physics;
+        calculateAverage();
+        this.isPass = false;
+
+    }
+    public void addBulkExamNote(int math, int physics, int chemistry){
+        if (math >= 0 && math <= 100)
+            this.math.note = math;
+
+        if (physics >= 0 && math <= 100)
+            this.physics.note = physics;
+
+        if (chemistry >= 0 && chemistry <= 100)
+            this.chemistry.note = chemistry;
+    }
+    public void calculateAverage(){
+        this.average = (this.chemistry.note + this.physics.note + this.math.note) / 3;
+    }
+    public void isPass(){
+        if (this.math.note == 0 || this.chemistry.note == 0 || this.physics.note == 0){
+            System.out.println("Notes not fully entered");
+        }else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Average : " + this.average);
+            if (this.isPass){
+                System.out.println("Class Passed.");
+            }else {
+                System.out.println("Failed in Class.");
+            }
+        }
+    }
+    public boolean isCheckPass(){
+        calculateAverage();
+        return this.average > 55;
+    }
+    public void printNote(){
+        System.out.println("-------------------------------");
+        System.out.println("Student : " + this.name);
+        System.out.println("Mathematic Note : " + this.math.note);
+        System.out.println("Physics Note : " + this.physics.note);
+        System.out.println("Chemistry Note : " + this.chemistry.note);
+    }
+}
