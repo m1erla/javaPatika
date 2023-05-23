4d71ac5cd7658b54bc2e9c77ada9cadbf371c03d
Furkan Karakus <furkankarakus01@hotmail.com>
2023-05-23 21:54:23 +0300

    Final Project For Java101 is Done!

---
+++ src/WeekTwo/TryAgain/Main.java
@@ -0,0 +1,11 @@
+package WeekTwo.TryAgain;
+
+public class Main {
+    public static void main(String[] args) {
+        // Created New Method
+         MineSweeper mineSweeper = new MineSweeper(5,5);
+         // You Can Get Output From This Method.
+         mineSweeper.output();
+
+    }
+}
